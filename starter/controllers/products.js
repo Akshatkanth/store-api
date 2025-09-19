7344b01d4d77a5af4c56a3d8efd3b1a3c64629cc
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-19 13:46:54 +0530

    express async errors

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -2,6 +2,7 @@
 //in static one we majorly test our api and see if it works, 
 // then implement it to the one without the static
 const getAllProductsStatic = async (req, res)=>{
+    throw new Error('testing async errors')
     res.status(200).json({msg: 'products testing route'})
 }
 
