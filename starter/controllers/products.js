e03f568dd1ff6ff8361d7bbc56f2a24caac974db
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-19 13:33:56 +0530

    product routing

---
+++ starter/controllers/products.js
@@ -0,0 +1,15 @@
+
+//in static one we majorly test our api and see if it works, 
+// then implement it to the one without the static
+const getAllProductsStatic = async (req, res)=>{
+    res.status(200).json({msg: 'products testing route'})
+}
+
+const getAllProducts = async (req, res)=>{
+    res.status(200).json({msg: 'products route'})
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic,
+}
