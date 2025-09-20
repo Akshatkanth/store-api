b699f1d99123630170bb020300c915a1241f9f73
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-21 00:24:46 +0530

    query object

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -5,14 +5,22 @@ const Product = require('../models/product')
 // then implement it to the one without the static
 const getAllProductsStatic = async (req, res)=>{
     const products = await Product.find({
-        name:'vase table',
+        page:'2',
     })
     res.status(200).json({products, nbhits: products.length})
 }
 
 const getAllProducts = async (req, res)=>{
-    const products = await Product.find(req.query);
-    res.status(200).json({})
+    const {featured} = req.qeury
+    const queryObject = {} 
+    
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+
+    console.log(queryObject);
+    const products = await Product.find(queryObject);
+    res.status(200).json({products, nbhits: products.length})
 }
 
 module.exports = {
