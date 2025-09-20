60a5ddeeebf097c0aeb3ac37aacd606c17aab4ae
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-20 16:08:38 +0530

    api fixing

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -5,15 +5,17 @@ const Product = require('../models/product')
 // then implement it to the one without the static
 const getAllProductsStatic = async (req, res)=>{
     const products = await Product.find({
-        featured:true})
-    res.status(200).json({products})
+        name:'vase table',
+    })
+    res.status(200).json({products, nbhits: products.length})
 }
 
 const getAllProducts = async (req, res)=>{
-    res.status(200).json({msg: 'products route'})
+    const products = await Product.find(req.query);
+    res.status(200).json({})
 }
 
 module.exports = {
     getAllProducts,
-    getAllProductsStatic,
+    getAllProductsStatic, 
 }
