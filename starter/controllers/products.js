c247eec0ad8cfaad79b6ba470f58ad714b8fd554
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-20 13:18:15 +0530

    populate js and mongoose find feature

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,9 +1,12 @@
+const Product = require('../models/product')
+
 
 //in static one we majorly test our api and see if it works, 
 // then implement it to the one without the static
 const getAllProductsStatic = async (req, res)=>{
-    throw new Error('testing async errors')
-    res.status(200).json({msg: 'products testing route'})
+    const products = await Product.find({
+        featured:true})
+    res.status(200).json({products})
 }
 
 const getAllProducts = async (req, res)=>{
