18e661e27cf0d513f19aa0e9f526935797cc03e0
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-21 13:59:11 +0530

    filter/search by name

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -4,22 +4,29 @@ const Product = require('../models/product')
 //in static one we majorly test our api and see if it works, 
 // then implement it to the one without the static
 const getAllProductsStatic = async (req, res)=>{
+    const search = 'ab'
     const products = await Product.find({
-        page:'2',
+        name: {$regex:search, $options: 'i'},
     })
     res.status(200).json({products, nbhits: products.length})
 }
 
 const getAllProducts = async (req, res)=>{
-    const {featured, company} = req.query
+    const {featured, company, name} = req.query
     const queryObject = {} 
     
+
+    //user can filter product from these three ifs: 
     if(featured){
         queryObject.featured = featured === 'true' ? true : false;
     }
 
     if(company){
         queryObject.company = company
+    }
+
+    if(name){
+        queryObject.name = {$regex:name, $options: 'i'}
     }
     
     console.log(queryObject);
