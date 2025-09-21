351f058130012242c13999e1f859a85a1798db91
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-21 18:52:41 +0530

    complete

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -54,10 +54,21 @@ const getAllProducts = async (req, res)=>{
             '<=' : '$lte',
         }
         const regEx = /\b(<|>|>=|=|<|<=)\b/g
-        let filters = numericFilters.replace(regEx, (match)=>`-${operatorMap[match]}-`)
-        console.log(filters)
-    }
+        let filters = numericFilters.replace(
+            regEx, 
+            (match)=>`-${operatorMap[match]}-`)
 
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item)=>{
+            const [field, operator, value] = item.split('-')
+            if(options.includes(field)){
+                queryObject[field] = {[operator]:Number(value)}
+
+            }
+        })
+    }
+    console.log(queryObject)
+    
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page - 1)* limit;
